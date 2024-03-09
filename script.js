c148ad8bf0299a39e2ab6b44e8f67113f2b36533
Durga Prasad Kakileti <durgaprasadkakileti@gmail.com>
2024-03-09 12:02:45 +0530

    Uploaded

---
+++ script.js
@@ -0,0 +1,13 @@
+// Smooth scrolling for navigation links
+document.querySelectorAll('.nav-link').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        const target = document.querySelector(this.getAttribute('href'));
+
+        window.scrollTo({
+            top: target.offsetTop,
+            behavior: 'smooth'
+        });
+    });
+});
